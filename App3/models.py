b7cd16acac2208af1093da85f7caa12718460218
Salvador Inetti <salvadorinetti@gmail.com>
2023-04-08 13:25:56 -0300

    como el 6to comitaje muchacho

--- App3/models.py
+++ App3/models.py
@@ -1,34 +1,39 @@
 from django.db import models
 
 # Create your models here.
-class Jugador(models.Model):
-    
-    nombre = models.CharField(max_length=200)
-    apellido = models.CharField(max_length=50)
-    edad = models.IntegerField(default=0)
-    equipo = models.CharField(max_length=200, default="Sin Equipo")
-    titular = models.BooleanField()
-
-
-    def __str__(self):
-        return f"Nombre: {self.nombre} {self.apellido} - Edad: {self.edad} - Posición: {self.posicion})"
-        
 class Equipo(models.Model):
-    
     nombre_equipo = models.CharField(max_length=50)
-    jugadores = models.CharField(max_length=200, default='')
+    region = models.CharField(max_length=50)
     fecha_fundacion = models.DateField()
 
     def __str__(self):
-        return f"Equipo: {self.nombre_equipo} - Jugadores: {self.jugadores} - Fecha de transferencia: {self.fecha_de_transferencia}"
+        return f"Equipo: {self.nombre_equipo} - Region: {self.region} - Fecha de fundación: {self.fecha_fundacion}"
 
-class Representante(models.Model):
-    
-    nombre = models.CharField(max_length=100)
-    sitio_web = models.URLField()
-    jugadores_contratados = models.ForeignKey(Jugador, on_delete=models.CASCADE)
+
+class Jugador(models.Model):
+    POSICION_CHOICES = (
+        ('g', 'Golero'),
+        ('d', 'Defensa'),
+        ('m', 'Mediocampista'),
+        ('f', 'Delantero'),
+    )
+
+    nombre = models.CharField(max_length=200)
+    apellido = models.CharField(max_length=50)
+    edad = models.IntegerField(default=0)
+    equipo = models.ForeignKey(Equipo, on_delete=models.CASCADE, related_name='jugadores')
+    posicion = models.CharField(max_length=1, choices=POSICION_CHOICES)
+    titular = models.BooleanField()
 
     def __str__(self):
-        return f"Nombre: {self.nombre} - Página web: {self.sitio_web} - Posee el contrato total: {self.contrato_total}"
+        titular_str = "Titular" if self.titular else "Suplente"
+        return f"Nombre: {self.nombre} {self.apellido} - Edad: {self.edad} - Equipo: {self.equipo.nombre_equipo} - Posición: {self.get_posicion_display()} - {titular_str}"
 
-    
+class Representante(models.Model):
+    nombre = models.CharField(max_length=100)
+    sitio_web = models.URLField()
+    jugadores_contratados = models.ManyToManyField(Jugador)
+
+    def __str__(self):
+        jugadores_str = ", ".join(jugador.nombre for jugador in self.jugadores_contratados.all())
+        return f"Nombre: {self.nombre} - Página web: {self.sitio_web} - Jugadores contratados: {jugadores_str}"
