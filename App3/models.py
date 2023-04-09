9c774a9c83b5c7cd6b7f356dfe8c667c92e26ab3
Salvador Inetti <salvadorinetti@gmail.com>
2023-04-09 20:45:51 -0300

    6to comitaje o por ahi

--- App3/models.py
+++ App3/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Equipo(models.Model):
+    
     nombre_equipo = models.CharField(max_length=50)
     region = models.CharField(max_length=50)
     fecha_fundacion = models.DateField()
@@ -11,7 +12,8 @@ class Equipo(models.Model):
 
 
 class Jugador(models.Model):
-    POSICION_CHOICES = (
+    
+    posiciones = (
         ('g', 'Golero'),
         ('d', 'Defensa'),
         ('m', 'Mediocampista'),
@@ -22,7 +24,7 @@ class Jugador(models.Model):
     apellido = models.CharField(max_length=50)
     edad = models.IntegerField(default=0)
     equipo = models.ForeignKey(Equipo, on_delete=models.CASCADE, related_name='jugadores')
-    posicion = models.CharField(max_length=1, choices=POSICION_CHOICES)
+    posicion = models.CharField(max_length=1, choices=posiciones)
     titular = models.BooleanField()
 
     def __str__(self):
@@ -30,10 +32,12 @@ class Jugador(models.Model):
         return f"Nombre: {self.nombre} {self.apellido} - Edad: {self.edad} - Equipo: {self.equipo.nombre_equipo} - Posición: {self.get_posicion_display()} - {titular_str}"
 
 class Representante(models.Model):
+    
     nombre = models.CharField(max_length=100)
     sitio_web = models.URLField()
     jugadores_contratados = models.ManyToManyField(Jugador)
 
     def __str__(self):
-        jugadores_str = ", ".join(jugador.nombre for jugador in self.jugadores_contratados.all())
-        return f"Nombre: {self.nombre} - Página web: {self.sitio_web} - Jugadores contratados: {jugadores_str}"
+        lista_jugadores = list(self.jugadores_contratados.all())
+        jugadoreslol = ", ".join(f"{jugador.nombre} {jugador.apellido}" for jugador in lista_jugadores)
+        return f"Nombre: {self.nombre} - Página web: {self.sitio_web} - Jugadores contratados: {jugadoreslol}"
